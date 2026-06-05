e8de3dd8d690bc88355eee1190b73f64915e3d44
Anton Sorokin <antonsorokin238@gmail.com>
2026-06-05 19:37:58 +0300

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        var q = new PriorityQueue<Integer>();
+
+        for (var n : nums) {
+            q.offer(n);
+
+            if (q.size() > k) q.poll();
+        }
+
+        return q.poll();
+    }
+}
