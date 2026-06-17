94b4f92cbfd3e3afbe196c37876b7a52e4ff2ffb
Anton Sorokin <antonsorokin238@gmail.com>
2026-06-17 19:57:35 +0300

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    int max = 0;
+    public int countSubstrings(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            expand(s, i, i);
+            expand(s, i, i+1);
+        }
+
+        return max;
+    }
+
+    void expand(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            max++;
+            l--;
+            r++;
+        }
+    }
+}
